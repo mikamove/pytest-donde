2a616d26334acdc9a8bc1a3f3b26b63c0e939143
Clemens Löbner <17496175+mikamove@users.noreply.github.com>
2023-10-01 11:49:18 +0200

    refactored (cont)

--- tests/test_index_mapper.py
+++ tests/test_index_mapper.py
@@ -7,9 +7,17 @@ from pytest_donde.index_mapper import IndexMapper
 def test_index_mapper():
     m = IndexMapper()
     assert m.to_index('b') == 0
-    assert m.to_index('a') == 1
-    assert m.to_index('a') == 1
-    assert m.to_index('b') == 0
-    assert m.to_index('c') == 2
+    assert m.index_to_val() == {0: 'b', }
 
-    assert m.index_to_val() == {0: 'b', 1: 'a', 2: 'c'}
+    assert m.to_index('a') == 1
+    assert m.index_to_val() == {0: 'b', 1: 'a', }
+
+    assert m.to_index('a') == 1
+    assert m.index_to_val() == {0: 'b', 1: 'a', }
+
+    assert m.to_index('b') == 0
+    assert m.index_to_val() == {0: 'b', 1: 'a', }
+
+    assert m.to_index('c') == 2
+    assert m.index_to_val() == {0: 'b', 1: 'a', 2: 'c', }
+
